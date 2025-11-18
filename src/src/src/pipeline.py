0831845bb0e79729294fface00da1f958cab2ba8
pranesh prabaharan <praneshprabaharan@gmail.com>
2025-11-18 14:41:52 +0530

    Update pipeline.py

--- src/src/src/pipeline.py
+++ src/src/src/pipeline.py
@@ -1,1 +1,10 @@
+from yolo_detector import detect_objects
+from lstm_model import ActivityLSTM
+
+def run_pipeline():
+    # Placeholder
+    print("Pipeline ready.")
+
+if __name__ == "__main__":
+    run_pipeline()
 
